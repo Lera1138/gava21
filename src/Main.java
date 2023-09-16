e8fb290cfc76672ea246385d1dbc3c17105da80b
Lera <lera1138@yandex.ru>
2023-09-16 21:46:57 +0300

    change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+
+
+        System.out.println();
+        System.out.println(service.calculate(150000));
+
+        System.out.println();
+        System.out.println(service.calculate(2000));
     }
 }
